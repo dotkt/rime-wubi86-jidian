11e5c7288e29c1246123823030f3a1e79aa79d41
dotkt <holiday_life@outlook.com>
2022-10-06 16:41:32 +0800

    添加obsidian的过期语法

--- rime.lua
+++ rime.lua
@@ -65,10 +65,12 @@ function date_translator(input, seg)
     if (input == "date") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("📅 %Y-%m-%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
+        -- 📅 2022-10-06
     end
 
     -- 输入时间
